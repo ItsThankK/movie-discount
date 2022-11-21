a1a08ec469fcb2e298ad3bc92f653168214cea3a
itsthankk <tkingsway3@gmail.com>
2022-11-21 19:21:22 +0100

    add extra exclamation

--- learnable.js
+++ learnable.js
@@ -4,6 +4,6 @@ if (age < 15) {
   console.log("You are eligible for this discount ):\n");
 }
 else {
-  console.log("You are not eligible for this discount!\n");
+  console.log("You are not eligible for this discount!!!\n");
 }
 console.log("Thanks for your patronage. Enjoy the movie, Wakanda forever!");
